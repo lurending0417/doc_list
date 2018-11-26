1c8eb616aa30b2ff82906b5dd244fd0e7674f4be
e <971649840@qq.com>
2018-11-26 15:02:17 +0800

    更新

--- tools.js
+++ tools.js
@@ -51,3 +51,60 @@ let getStorage = function(key,defaultValue){
 let setStorage = function(key,obj){
 	localStorage[key] = JSON.stringify(obj);
 }
+
+// 比较基本类型（NaN,Infinity……）
+let is = function(x, y) {
+    if (x === y) {
+        return x !== 0 || y !== 0 || 1 / x === 1 / y
+    } else {
+        return x !== x && y !== y
+    }
+}
+
+// 比较两个对象
+let shallowEqual = function(objA, objB) {
+    if (is(objA, objB)) return true
+
+    if (typeof objA !== 'object' || objA === null ||
+        typeof objB !== 'object' || objB === null) {
+        return false
+    }
+
+    const keysA = Object.keys(objA)
+    const keysB = Object.keys(objB)
+
+    if (keysA.length !== keysB.length) return false
+
+    for (let i = 0; i < keysA.length; i++) {
+        if (!hasOwnProperty.call(objB, keysA[i]) ||
+            !is(objA[keysA[i]], objB[keysA[i]])) {
+            return false
+        }
+    }
+
+    return true
+}
+
+const query2Object = str => {
+	if (!str) {
+		return {}
+	}
+	if (/\?/.test(str)) {
+		str = str.replace(/.*\?/g, "")
+	}
+	return str.split("&").reduce((last, now) => {
+		const rs = now.split("=");
+		if (rs.length == 2) {
+			last[rs[0]] = decodeURIComponent(rs[1]);
+		}
+		return last;
+	}, {})
+}
+
+const object2Query = body => {
+	return Object.keys(body || {}).reduce((last, key) => {
+		let v = typeof body[key] == 'object' ? JSON.stringify(body[key]) : body[key];
+		last += (last ? "&" : "") + key + "=" + (v);
+		return last;
+	}, "") || ""
+}
